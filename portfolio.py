becebeb8519074011010b0897fc9bef24c6c4bbc
keshavbharadwaj <keshavbharadwaj98@gmail.com>
2023-12-20 01:09:52 -0500

    first commit

---
+++ portfolio.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('base_page.html')
+
+@app.route('/home')
+def home_page():
+    return render_template('base_page.html')
+
+@app.route('/work')
+def work():
+    return render_template('work_experience.html')
+
+@app.route('/research')
+def research():
+    return render_template('research_experience.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/patents')
+def patents():
+    return render_template('patents.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
